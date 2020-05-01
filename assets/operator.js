92c6a161a1aebf630cbb36eeded618f61b0860c1
hieungochoang97 <hieungochoang97@gmail.com>
2020-05-01 16:18:03 +0700

    Operator

---
+++ assets/operator.js
@@ -0,0 +1,15 @@
+/**
+ * Toán tử số học - Arithmetic
+ * Toán tử gán - Asignment
+ * Toán tử so sánh - Comparision
+ * Toán tử logic - Logical
+ */
+
+var fullName = 'Hoang Ngoc Hieu';
+
+var a = 1;
+var b = 2;
+
+if (a > b) {
+    console.log('dung');
+} console.log('sai');
