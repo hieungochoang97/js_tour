f4337efb6207b404f812ae6f5b5f27cc018d7acf
hieungochoang97 <hieungochoang97@gmail.com>
2020-05-06 22:22:50 +0700

    Comparison Operator

---
+++ assets/comparison-operator.js
@@ -0,0 +1,19 @@
+/**
+ * = bằng
+ * != không bằng
+ * > lớn hơn
+ * < nhỏ hơn
+ * >= lớn hơn hoặc bằng
+ * <= nhỏ hơn hoặc bằng
+ */
+
+ var a = 10;
+ var b = 20;
+
+ if (a > b) {
+     console.log('a lon hon b')
+ } else console.log('a nho hon b');
+
+if (a < b) {
+    console.log('a nho hon b');
+} else console.log('a lon hon b');
