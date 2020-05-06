9f6d3b361fc2bc50ca6d81b972c2f18b8c229121
hieungochoang97 <hieungochoang97@gmail.com>
2020-05-06 23:29:35 +0700

    If else

---
+++ assets/if-else.js
@@ -0,0 +1,38 @@
+/**
+ * If - Else
+ */
+
+ /** Cac gia tri khi convert sang boolean la false:
+  * 0
+  * false
+  * '' - ""
+  * undefined
+  * NaN
+  * null
+  */
+
+var isSuccess = 1 < 2;
+
+if (isSuccess) {
+
+    console.log('Dung');
+
+} else {
+
+    console.log('Sai');
+
+}
+
+/**--------------------- */
+
+var fullName = 'Ngoc Hieu';
+
+if (fullName) {
+
+    console.log('Dung');
+
+} else {
+
+    console.log('Sai');
+
+}
