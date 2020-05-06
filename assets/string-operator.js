efd9cddd8a52e2aa41986e00961ec63bce8278e8
hieungochoang97 <hieungochoang97@gmail.com>
2020-05-06 22:07:55 +0700

    Operator String

---
+++ assets/string-operator.js
@@ -0,0 +1,13 @@
+/**
+ * Khai báo biến firstName, lastName và gán giá trị là Họ và Tên bất kì.
+Khai báo biến fullName có giá trị là kết quả của firstName nối với lastName. Giữa firstName và lastName cần có một khoảng trắng (sử dụng một chuỗi chứa dấu cách)
+ */
+
+ // Toan tu noi chuoi.
+
+ var firstName = 'Hieu';
+ var lastName = 'Hoang';
+
+ var fullName = firstName + ' ' + lastName;
+
+ console.log(fullName);
