481ed2b4be6a434d5ae2382898e72264bbcc288d
hieungochoang97 <hieungochoang97@gmail.com>
2020-05-06 21:23:03 +0700

    Operator homework

---
+++ assets/operator-homework.js
@@ -0,0 +1,17 @@
+/**
+ * Viết chương trình tính tổng 2 số:
+
+Cho trước biến a, b là 2 số bất kỳ. Hãy viết chương trình tính tổng 2 số này.
+ */
+
+function run(a, b) {
+
+    c = a + b;
+
+    return c;
+}
+run(1, 2);
+
+console.log(run(1, 2));
+
+// console.log(c);
