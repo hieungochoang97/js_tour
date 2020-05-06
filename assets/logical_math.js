ed51762df363dca6b716c52b0b3e8589daacafec
hieungochoang97 <hieungochoang97@gmail.com>
2020-05-06 23:37:30 +0700

    Logical Math

---
+++ assets/logical_math.js
@@ -0,0 +1,27 @@
+/**
+ * Logical math:
+ * 1, && - And
+ * 2, || - Or
+ * 3, ! - Not
+ */
+
+ var a = 1;
+ var b = 2;
+ var c = 3;
+
+
+ // And - && - va
+ if (a > 0 && b > 0 && c > a) {
+     console.log('Dung');
+ }
+
+
+ // Or = || - hoac
+ if (a > 5 || b > 0) {
+     console.log('Dung');
+ } 
+
+ // Not - ! - phu dinh
+ if (!(a < 0)) {
+    console.log('Dung');
+} 
