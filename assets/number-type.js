9087531580704bdc156c1efc652d414c88c7867c
hieungochoang97 <hieungochoang97@gmail.com>
2020-05-09 00:04:22 +0700

    number type

---
+++ assets/number-type.js
@@ -0,0 +1,9 @@
+/**
+ * Tạo biến age lưu số tuổi bất kì
+Tạo biến pi để lưu số PI (làm tròn tới 2 số thập phân)
+Tạo biến bonusPoint gán giá trị là số điểm bạn sẽ đạt được nếu vượt qua toàn bộ test case của bài tập này
+ */
+
+ var age = 23;
+ var pi = 3.14;
+ var bonusPoint = 69;
