d2bc0933601df752488a0d5326d58347aa3d8590
hieungochoang97 <hieungochoang97@gmail.com>
2020-05-09 00:43:22 +0700

    Comparison operator

---
+++ assets/test14.js
@@ -0,0 +1,19 @@
+/**
+ * Kiểm tra biến bonusPoint có phải chứa giá trị là 50 hay không. Nếu đúng gán true cho result, sai gán false cho result.
+ */
+
+function run(bonusPoint) {
+    var result;
+
+    if (bonusPoint === 50) {
+        result = true;
+    } else {
+        result = false;
+    }
+
+    return result;
+}
+
+run(50);
+
+console.log(run(50));
