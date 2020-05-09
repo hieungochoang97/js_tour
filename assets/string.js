6162d2fb1e4c4512f87eb7e660618e93505c9811
hieungochoang97 <hieungochoang97@gmail.com>
2020-05-09 22:46:45 +0700

    String
    string, template string

---
+++ assets/string.js
@@ -0,0 +1,30 @@
+var fullName = 'Hoang Ngoc Hieu "Dang hoc lap trinh"'; // string
+
+console.log(fullName.length); // do dai chuoi
+
+var firstName = 'Hieu';
+var lastName = 'Hoang';
+
+console.log(`Toi la: ${firstName} ${lastName}`); // template strings
+
+/**Work 
+ * Tạo biến commentText để lưu nội dung bình luận sau: "Học chưa hiểu là học chưa đủ!" (bao gồm cả dấu ngoặc kép).
+Tạo biến authorName có giá trị Sơn Đặng:
+Tạo biến fullCommentText có giá trị bằng authorName nối với commentText, sử dụng template string để nối, giữa authorName và commentText cần nối thêm một khoảng trắng (một dấu cách).
+ * 
+ * 
+*/
+
+// var commentText = "\"Học chưa hiểu là học chưa đủ!\"";
+// var authorName = 'Sơn Đặng:';
+// var fullCommentText = `${authorName} ${commentText}`;
+
+// console.log(fullCommentText);
+
+/**
+ * Tạo biến commentText để lưu bình luận sau: Để hiển thị được chuỗi chứa dấu gạch chéo ngược (\) ta phải thêm dấu \ vào trước hoặc sau nó
+Sử dụng làm alert hoặc console.log để xem giá trị biến commentText
+ */
+
+ var commentText = 'Để hiển thị được chuỗi chứa dấu gạch chéo ngược (\\) ta phải thêm dấu \\ vào trước hoặc sau nó';
+ console.log(commentText);
