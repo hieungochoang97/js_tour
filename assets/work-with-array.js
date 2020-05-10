cc738648aab8d1baeb5b64bcc63c5eda3ede7658
hieungochoang97 <hieungochoang97@gmail.com>
2020-05-10 12:40:52 +0700

    Update work with array

--- assets/work-with-array.js
+++ assets/work-with-array.js
@@ -1,35 +1,35 @@
-// var languages = [
-//     'HTML',
-//     'CSS',
-//     'JAVASCRIPT'
-// ];
+var languages = [
+    'HTML',
+    'CSS',
+    'JAVASCRIPT'
+];
 
-// // toString
-// console.log(languages.toString());
+// toString
+console.log(languages.toString());
 
-// // join
-// console.log(languages.join(' - '));
+// join
+console.log(languages.join(' - '));
 
-// // pop - xoa element cuoi array tra ve phan tu da xoa
-// console.log(languages.pop());
+// pop - xoa element cuoi array tra ve phan tu da xoa
+console.log(languages.pop());
 
-// // push - them phan tu vao cuoi mang tra ve do dai mang
-// console.log(languages.push('Dart', 'Java', 'ABC'));
+// push - them phan tu vao cuoi mang tra ve do dai mang
+console.log(languages.push('Dart', 'Java', 'ABC'));
 
-// // shift - xoa phan tu dau va tra ve gia tri
-// console.log(languages.shift());
+// shift - xoa phan tu dau va tra ve gia tri
+console.log(languages.shift());
 
-// // unshift - them phan tu vao dau mang tra ve do dai mang
-// console.log(languages.unshift('IOS', 'ANDROID'));
+// unshift - them phan tu vao dau mang tra ve do dai mang
+console.log(languages.unshift('IOS', 'ANDROID'));
 
-// // splicing - xoa (vi tri con tro, vi tri xoa, them phan tu), chen
-// languages.splice(1, 0, 'TOOL');
+// splicing - xoa (vi tri con tro, vi tri xoa, them phan tu), chen
+languages.splice(1, 0, 'TOOL');
 
-// // concat - noi 2 mang
-// var languages2 = [
-//     'APP',
-//     'ACC'
-// ]
+// concat - noi 2 mang
+var languages2 = [
+    'APP',
+    'ACC'
+]
 
 // console.log(languages.concat(languages2));
 
@@ -48,20 +48,20 @@ Input: [1, 2, 3] - Output: '1,2,3'
 Input: ['Javascript', 'PHP'] - Output: 'Javascript,PHP'
  */
 
-// function arrayToString(input) {
-//     var result = input.toString();
-//     return result;
-// }
-// arrayToString([
-//     1,
-//     2,
-//     3
-// ]);
-// console.log(arrayToString([
-//     1,
-//     2,
-//     3
-// ]));
+function arrayToString(input) {
+    var result = input.toString();
+    return result;
+}
+arrayToString([
+    1,
+    2,
+    3
+]);
+console.log(arrayToString([
+    1,
+    2,
+    3
+]));
 
 /**
  * Cho trước biến input là array bất kì. Hãy chuyển đổi kiểu dữ liệu biến input sang chuỗi, mỗi giá trị của các phần tử được cách nhau bằng dấu ' - ', gán giá trị sau chuyển đổi cho biến result.
@@ -73,21 +73,21 @@ Input: ['Chicken', 'Tiger', 'Monkey'] - Output: 'Chicken - Tiger - Monkey'
  */
 
  
-// function arrayToString(input) {
-//     var result = input.toString();
-//     result = input.join(' - ');
-//     return result;
-// }
-// arrayToString([
-//     1,
-//     2,
-//     3
-// ]);
-// console.log(arrayToString([
-//     1,
-//     2,
-//     3
-// ]));
+function arrayToString(input) {
+    var result = input.toString();
+    result = input.join(' - ');
+    return result;
+}
+arrayToString([
+    1,
+    2,
+    3
+]);
+console.log(arrayToString([
+    1,
+    2,
+    3
+]));
 
 
 /**
@@ -102,19 +102,19 @@ Input: [6, 8] - Output: [8]
 Input: [1, 2, 3, 4] - Output: [1, 2]
  */
 
-// function run(anArray) {
-//     console.log(anArray.length);
-//     if (anArray.length < 3) {
-//         anArray.shift();
-//     } else if (anArray.length > 2) {
-//         anArray.pop();
-//         anArray.pop();
-//     }
-//     return anArray;
-// }
-// run([10, 10, 10, 10]);
-// var x = run([10, 10, 10, 10]);
-// console.log(x);
+function run(anArray) {
+    console.log(anArray.length);
+    if (anArray.length < 3) {
+        anArray.shift();
+    } else if (anArray.length > 2) {
+        anArray.pop();
+        anArray.pop();
+    }
+    return anArray;
+}
+run([10, 10, 10, 10]);
+var x = run([10, 10, 10, 10]);
+console.log(x);
 
 /**
  * Cho biến animals là mảng chứa tên các loài động vật. Hãy thêm tên các loài động vật khác vào mảng animals tuân thủ theo các yêu cầu sau:
@@ -136,3 +136,28 @@ function run(animals) {
 }
 run(['dog', 'cat']);
 console.log(run(['dog', 'cat']));
+
+/**
+ * Một website nọ có chức năng tải thêm (load more) tại trang danh sách sản phẩm. Cứ mỗi khi người dùng kéo con lăn chuột xuống (scroll down) cuối danh sách sản phẩm là sẽ tải thêm 10 sản phẩm.
+
+Diễn dải
+Về mặt logic xử lý phía sau giao diện web, bài toán trên sẽ sử dụng kỹ thuật nối mảng. Thứ tự thực hiện các công việc thường diễn ra như sau:
+
+Khi load trang web, tải sẵn một số lượng sản phẩm nhất định (mảng A)
+Khi scroll down cuối danh sách, tải thêm một số lượng sản phẩm nhất định (mảng B)
+Thực hiện nối mảng A và mảng B thành một mảng duy nhất sau đó hiển thị ra giao diện người dùng
+Lúc này chúng ta chưa xây dựng website như bài toán trên nêu ra. Tuy nhiên logic chúng ta sử dụng để giải bài tập này chính là logic chúng ta sẽ sử dụng để giải bài toán thực tế trên trong tương lai.
+
+Bài tập
+Cho biến products và newProducts là hai mảng chứa danh sách các sản phẩm. Mảng products được sử dụng để hiển thị sản phẩm ra giao diện người dùng. Mảng newProducts là mảng mới được trả về từ máy chủ.
+
+Yêu cầu: Hãy thực hiện nối mảng products và mảng newProducts với nhau và gán kết quả vào biến output.
+ */
+function joinTwoArrays(products, newProducts) {
+    var output;
+    output = products.concat(newProducts);
+    return output;
+}
+joinTwoArrays(['Sản phẩm 1'], ['Sản phẩm 2', 'Sản phẩm 3']);
+console.log(joinTwoArrays(['Sản phẩm 1'], ['Sản phẩm 2', 'Sản phẩm 3']));
+
