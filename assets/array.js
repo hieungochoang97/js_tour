5cea5f35448767a2522b09255043d92e7b7c4b1e
hieungochoang97 <hieungochoang97@gmail.com>
2020-05-10 14:49:16 +0700

    Update array
        forEach()
        every()
        some()
        find()
        filter()

--- assets/array.js
+++ assets/array.js
@@ -48,3 +48,88 @@ console.log(getLastElementOfArray([
     2,
     3
 ]));
+
+
+/** LAM VIEC VOI MANG P2 */
+/*
+    Array methods :
+    forEach()
+    every()
+    some()
+    find()
+    filter()
+    map()
+    reduce()
+*/
+
+var courses = [
+    {
+        id: 1,
+        name: 'Hoang Ngoc Hieu',
+        coin: 100000
+    },
+    {
+        id: 2,
+        name: 'Nguyen Minh Hieu',
+        coin: 20000
+    },
+    {
+        id: 3,
+        name: 'Trieu Van Tu',
+        coin: 50000
+    },
+    {
+        id: 4,
+        name: 'Xinzhao',
+        coin: 0
+    },
+    {
+        id: 5,
+        name: 'Yasuo',
+        coin: 10000000
+    },
+    {
+        id: 5,
+        name: 'Zed',
+        coin: 0
+    },
+    {
+        id: 6,
+        name: 'Nguyen Minh Hieu',
+        coin: 40000
+    }
+];
+
+courses.forEach(function(course) {
+    console.log(course);
+}) // call back
+
+console.log('========================');
+
+var isFree = courses.every(function(course, index) {
+    console.log(index);
+    return course.coin === 0;
+})
+console.log(isFree);
+
+console.log('========================');
+
+var isFree = courses.some(function(course, index) {
+    console.log(index);
+    return course.coin === 0;
+})
+console.log(isFree);
+
+console.log('========================');
+
+var course = courses.find(function(course, index) {
+    return course.name === 'Nguyen Minh Hieu';
+})
+console.log(course);
+
+console.log('========================');
+
+var course = courses.filter(function(course, index) {
+    return course.name === 'Nguyen Minh Hieu';
+})
+console.log(course);
