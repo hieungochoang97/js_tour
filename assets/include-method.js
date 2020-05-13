26cd028d49f8804ca46f475b8757345cf7b718db
hieungochoang97 <hieungochoang97@gmail.com>
2020-05-13 21:11:07 +0700

    Includes method

---
+++ assets/include-method.js
@@ -0,0 +1,9 @@
+//kiem tra xem tu trong chuoi
+
+var title = 'reponsive web disign';
+console.log(title.includes('web'));
+
+//
+
+var courses = ['javascrip', 'html', 'css'];
+console.log(courses.includes('html'));
