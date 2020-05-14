ce3765827c7302ab185a045b2a5564509600cbe1
hieungochoang97 <hieungochoang97@gmail.com>
2020-05-15 01:23:49 +0700

    Switch

---
+++ assets/switch.js
@@ -0,0 +1,16 @@
+var date = 9;
+
+
+switch (date) {
+    case 2:
+        console.log('day la thu 2');
+        break;
+    case 3:
+        console.log('day la thu 3');
+        break;
+    case 4:
+        console.log('day la thu 4');
+        break;
+    default:
+        console.log('no day');
+}
