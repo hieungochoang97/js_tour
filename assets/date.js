ab7ba4cdc168aa567981b5f3ffbfb715589bc574
hieungochoang97 <hieungochoang97@gmail.com>
2020-05-15 01:06:05 +0700

    Date

---
+++ assets/date.js
@@ -0,0 +1,13 @@
+// Date
+
+var date = new Date ();
+
+console.log(date);
+
+var year = date.getFullYear();
+var month = date.getMonth() + 1;
+var day = date.getDate();
+
+console.log(`${day}/${month}/${year}`);
+
+
