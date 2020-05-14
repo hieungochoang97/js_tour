c4944d8b7390c974cff3bdf667e50de94f6e67d4
hieungochoang97 <hieungochoang97@gmail.com>
2020-05-15 01:12:17 +0700

    Update if else

--- assets/if-else.js
+++ assets/if-else.js
@@ -36,3 +36,18 @@ if (fullName) {
     console.log('Sai');
 
 }
+
+
+// Câu lệnh rẽ nhánh IF - ELSE
+
+var date = 3;
+
+if (date === 2) {
+    console.log('day la thu 2');
+} else if (date === 3) {
+    console.log('day la thu 3');
+} else if (date === 4) {
+    console.log('day la thu 4');
+} else {
+    console.log('No day');
+}
