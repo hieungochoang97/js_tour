468a42e12802688e9d91af43156fe09b5b968945
hieungochoang97 <hieungochoang97@gmail.com>
2020-05-19 20:36:05 +0700

    For/in loop

--- assets/loop.js
+++ assets/loop.js
@@ -15,18 +15,53 @@
 //  }
 
 
-var myArray = [
-    'Javascript',
-    'Html',
-    'Css',
-    'Dart',
-    'Ruby',
-    'Python'
-];
+// var myArray = [
+//     'Javascript',
+//     'Html',
+//     'Css',
+//     'Dart',
+//     'Ruby',
+//     'Python'
+// ];
+// var arrayLength = myArray.length;
+// for (i = 0; i < arrayLength; i++) {
+//     console.log(myArray[i]);
+// }
 
-var arrayLength = myArray.length;
+/**=========================================== */
 
-for (i = 0; i < arrayLength; i++) {
-    console.log(myArray[i]);
+// For/in loop
+
+var myInfo = {
+    name: 'Ngoc hieu',
+    age: 23
+}
+for (var key in myInfo) {
+    console.log(key);
+    console.log(myInfo[key]);
 }
 
+/**=========================================== */
+
+var languages = [
+    'JS',
+    'HTML',
+    'CSS'
+];
+
+for (var key in languages) {
+    console.log(key);
+    console.log(languages[key]);
+}
+
+
+/**=========================================== */
+
+var myString = 'Javascript';
+
+console.log(myString);
+
+for (var key in myString) {
+    // console.log(key);
+    console.log(myString[key]);
+}
